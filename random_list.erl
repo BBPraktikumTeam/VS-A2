607ec44942ed3dc1c0c5459d65bb389b0f7002e8
Oliver <oliver.behncke@googlemail.com>
2012-11-04 17:26:25 +0100

    random list elements implemented

---
+++ random_list.erl
@@ -0,0 +1,15 @@
+-module(random_list).
+-export([get_first_n/2]).
+
+get_first_n(N,List) when is_integer(N) and is_list(List) ->
+    Length = length(List),
+    if (N < 1) or (N > Length) -> {error, n_bigger_than_list};
+       true -> {ok,lists:sublist(random_ordering(List),N)}
+    end.
+
+random_ordering(List) when is_list(List) ->
+    TupleList=lists:map(fun(X)-> {random:uniform(),X} end,List),
+    SortedTupleList=lists:keysort(1,TupleList),
+    lists:map(fun({_,X})-> X end,SortedTupleList).
+		     
+    
