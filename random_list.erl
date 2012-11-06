4e2836f643571792c6d782714cc32c5e60ddaccd
kaib <kai.bielenberg@haw-hamburg.de>
2012-11-06 20:11:57 +0100

    random list without ok in return

--- random_list.erl
+++ random_list.erl
@@ -4,7 +4,7 @@
 get_first_n(N,List) when is_integer(N) and is_list(List) ->
     Length = length(List),
     if (N < 1) or (N > Length) -> {error, n_bigger_than_list};
-       true -> {ok,lists:sublist(random_ordering(List),N)}
+       true -> lists:sublist(random_ordering(List),N)
     end.
 
 random_ordering(List) when is_list(List) ->
