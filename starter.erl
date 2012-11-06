968ae74fee04cbb19b464d7dd0937f47a0857f12
Oliver <oliver.behncke@haw-hamburg.de>
2012-11-06 10:08:07 +0100

    added logging to starter

--- starter.erl
+++ starter.erl
@@ -1,85 +1,90 @@
 -module(starter).
 -compile(export_all).
--record(state,{koordinatorname,team,gruppe,nameservicenode}).
+-record(state,{koordinatorname,team,gruppe,nameservicenode,starternr}).
 
-%% %%%%%%%%%%%%%%%%%
-% TODO:
-% Logger einbauen
-% wie bekommt der Starter ne eindeutige Nummer?
-%% %%%%%%%%%%%%%%%%%
+start()-> start(1).
 
-start()-> spawn(fun init/0).
+start(AnzahlStarter) when AnzahlStarter > 0 -> lists:map(fun(X)->spawn(fun()->init(X) end) end,lists:seq(1,AnzahlStarter)).
 
-init()->
+init(Starternr)->
     {ok,ConfigListe} = file:consult("ggt.cfg"),
     {ok, Praktikumsgruppe} = werkzeug:get_config_value(praktikumsgruppe,ConfigListe),
     {ok, Teamnummer} = werkzeug:get_config_value(teamnummer, ConfigListe),
     {ok, Nameservicenode} = werkzeug:get_config_value(nameservicenode, ConfigListe),
     {ok, Koordinatorname} = werkzeug:get_config_value(koordinatorname, ConfigListe),
-    S=#state{koordinatorname=Koordinatorname,nameservicenode=Nameservicenode,team=Teamnummer,gruppe=Praktikumsgruppe},
+    S=#state{koordinatorname=Koordinatorname,nameservicenode=Nameservicenode,team=Teamnummer,gruppe=Praktikumsgruppe,starternr=Starternr},
     case get_koordinator(S) of
 	{ok,Koordinator}->
+	    log("Asking Koordinator for steeringval",Starternr),
 	    Koordinator ! {getsteeringval,self()},
 	    loop_steeringval(S);
 	{error,Reason}->
-	    io:format("Error getting Koordinator: ~p\nExpecting new command\n",[Reason]),
+	    log(lists:concat(["Error getting Koordinator: ",Reason]),Starternr),
 	    loop_commands(S)
     end.
     
     
-loop_steeringval(S=#state{koordinatorname=Koordinatorname,team=Team,gruppe=Gruppe,nameservicenode=Nameservicenode})->    
+loop_steeringval(S=#state{koordinatorname=Koordinatorname,team=Team,gruppe=Gruppe,nameservicenode=Nameservicenode,starternr=Starternr})->    
     receive
 	{steeringval,ArbeitsZeit,TermZeit,GGTProzessnummer} ->
-	    lists:map(fun(X)-> ggt:start(ArbeitsZeit,TermZeit,X,Gruppe,Team,Nameservicenode,Koordinatorname,1) end, lists:seq(1,GGTProzessnummer));
+	    log(lists:concat(["Starting ",GGTProzessnummer," processes with ",ArbeitsZeit," working delay and ",TermZeit," time to termination"]),Starternr),
+	    lists:map(fun(X)-> ggt:start(ArbeitsZeit,TermZeit,X,Gruppe,Team,Nameservicenode,Koordinatorname,Starternr) end, lists:seq(1,GGTProzessnummer));
 	kill -> 
-	    io:format("Received kill command\n"),
-	    exit(normal);
-        Any -> 
-	    io:format("Not expecting: ~p\n",[Any]),
+	    terminate(S);
+        _Any -> 
+	    log("Unexpected message",Starternr),
 	    loop_steeringval(S)
     end.
 
-loop_commands(S)->
+loop_commands(S=#state{starternr=Starternr})->
     receive
 	askagain -> 
 	    case get_koordinator(S) of
 		{ok,Koordinator}->
+		    log("Asking Koordinator for steeringval",Starternr),
 		    Koordinator ! {getsteeringval,self()},
 		    loop_steeringval(S);
 		{error,Reason}->
-		    io:format("Error getting Koordinator: ~p\nExpecting new command\n",[Reason]),
+		    log(lists:concat(["Error getting Koordinator: ",Reason]),Starternr),
+		    log("Expecting new command",Starternr),
 		    loop_commands(S)
 	    end;
         kill -> 
-	    terminate();
-	Any ->
-	    io:format("Not expecting: ~p\n",[Any]),
+	    terminate(S);
+	_Any ->
+	    log("Unexpected Command",Starternr),
 	    loop_commands(S)
     end.
 			  
-get_koordinator(#state{koordinatorname=Koordinatorname,nameservicenode=Nameservicenode})->    
+get_koordinator(S=#state{koordinatorname=Koordinatorname,nameservicenode=Nameservicenode,starternr=Starternr})->    
     net_adm:ping(Nameservicenode),
     receive
 	kill ->
-	    terminate();
+	    terminate(S);
 	pang -> 
-	    io:format("Cannot reach nameserivenode!\n"),
+	    log("Cannot reach nameserivenode!",Starternr),
 	    {error,no_nameservicenode};
 	pong -> 
 	    Nameservice = global:whereis_name(nameservice),
 	    Nameservice ! {self(),{lookup,Koordinatorname}},
 	    receive
 		not_found ->
-		    io:format("Koordinator not found.\n"),
+		    log("Koordinator not found",Starternr),
 		    {error,no_koordinator};
 		kill ->
-		    terminate();
+		    terminate(S);
 		Koordinator -> 
-		    io:format("Found koordinator: ~p.\n",[Koordinator]),
+		    log("Found koordinator",Starternr),
 		    {ok,Koordinator}
 	    end
     end.    
-    
-terminate()->
-    	    io:format("Received kill command\n"),
+
+
+terminate(#state{starternr=Starternr})->
+    	    log("Received kill command",Starternr),
 	    exit(normal).
+
+log(Message,Starternr)->
+    Name = lists:concat(["ggt",Starternr,"@",net_adm:localhost()]),
+    NewMessage = lists:concat([Name,werkzeug:timeMilliSecond()," | ",Message,io_lib:nl()]),
+    werkzeug:logging(lists:concat([Name,".log"]),NewMessage).
